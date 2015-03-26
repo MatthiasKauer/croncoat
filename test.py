50494776b0ee94954a56a6b972b693b7681b2702
unknown <matthias.kauer@N-2011-122.TUMCREATE.LOCAL>
2015-03-26 18:05:38 +0800

    tried to get expiring test running; still issues w/ stdout/stderr however; adding them via pipes makes the thing not exit anymore => whole idea moot

---
+++ test.py
@@ -0,0 +1,31 @@
+import time
+import argparse
+parser = argparse.ArgumentParser(description='Throw error or not')
+parser.add_argument('--err', dest='err', action='store_const',
+                   default=False, const=True, help='should I throw error')
+
+parser.add_argument('--exitcode', dest='code', action='store_const',
+                   default=False, const=True, help='should I change exit code')
+
+parser.add_argument('--sleep', dest='sleep', action='store_const',
+                   default=False, const=True, help='should I sleep for 1min')
+
+args = parser.parse_args()
+print("parsed all")
+print("err " + str(args.err))
+
+with open('log.txt', 'a') as f:
+        f.write("asdf\n")
+
+if (args.sleep):
+        print "sleeping"
+        time.sleep(5)
+
+if(args.err):
+        print "raising exception"
+        raise Exception("default Exception")
+
+if(args.code):
+        exit(1)
+
+exit(0)
