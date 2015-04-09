48a59e6aba64f167536f267df6ae306194bf9af1
Matthias Kauer <mk.software@zuez.org>
2015-04-09 23:45:28 +0800

    mail login works now

--- cronwrap/cw/mailbackend.py
+++ cronwrap/cw/mailbackend.py
@@ -1,17 +1,27 @@
 from smtplib import SMTP_SSL
+import os
 import ConfigParser
 
 class MailBackend(object):
     def __init__(self):
         self.smtp = SMTP_SSL()
-        self.cfg = ConfigParser.ConfigParser()
-        self.cfg.read(['~/.cronwrap.cfg'])
+        self.cfg = ConfigParser.SafeConfigParser()
+        fname = os.path.expanduser('~/.cronwrap.ini')
+        self.cfg.read(fname)
 
-        server = self.cfg['Mail']['smtpserver']
-        port = self.cfg['Mail']['smtpport']
-        mailuser = self.cfg['Mail']['user']
-        mailpass = self.cfg['Mail']['pass']
-        self.fromaddr = self.cfg['Mail']['fromaddr']
+        print(self.cfg.sections())
+        #  print(self.cfg.options('Mail'))
+        server = self.cfg.get('Mail', 'smtpserver')
+        port = self.cfg.get('Mail', 'smtpport')
+        mailuser = self.cfg.get('Mail','user')
+        mailpass = self.cfg.get('Mail','pass')
+        self.fromaddr = self.cfg.get('Mail', 'fromaddr')
+        #  if False: #appears to be python 3 only
+        #  server = self.cfg['Mail']['smtpserver']
+        #  port = self.cfg['Mail']['smtpport']
+        #  mailuser = self.cfg['Mail']['user']
+        #  mailpass = self.cfg['Mail']['pass']
+        #  self.fromaddr = self.cfg['Mail']['fromaddr']
         print(server, port, mailuser, mailpass, self.fromaddr)
         
         self.smtp.connect(server, port)
