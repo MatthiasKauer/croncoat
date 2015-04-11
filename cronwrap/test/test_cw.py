756b408c773402f6b2d68afaf1028109d6c75a5e
Matthias Kauer <mk.software@zuez.org>
2015-04-11 17:30:27 +0800

    added: test folder

---
+++ cronwrap/test/test_cw.py
@@ -0,0 +1,18 @@
+'''
+Created on 11 Apr, 2015
+
+@author: matthias
+'''
+import unittest
+import cronwrap.scripts
+
+class Test(unittest.TestCase):
+
+
+    def test_expiring(self):
+        
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test_expiring']
+    unittest.main()
