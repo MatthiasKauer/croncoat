84a7eacca451fd75f6f518b586b5d169f1ac7bb2
Matthias Kauer <mk.software@zuez.org>
2017-06-04 20:55:53 +0200

    experiment a bit; then find that there is no std support for timeout

--- croncoat/cc/expiringcommand.py
+++ croncoat/cc/expiringcommand.py
@@ -23,45 +23,39 @@ def alarm_handler(signum, frame):
 
 class ExpiringCommand(object):
     #http://stackoverflow.com/questions/1191374/subprocess-with-timeout
-    
+
     def savereturn(self):
         self.run_time = time.time() - self.start_time
         self.returncode = self.p.returncode;
 
-        #  self.stderr = "asdf"
-        #  self.stdout = self.p.stdout.read()
-        #  with open(self.outfile) as fout:
-        #  with open(self.outfile) as fout, open(self.errfile) as ferr:
-        #      self.stdout = fout.read()
-        #      self.stderr = ferr.read()
-        #      print("ExpSignal; stdout: %s %s" % (self.stdout, self.stderr))
-
     def __init__(self, command, timeout):
-#         self.cmd = command.split()
-        #  print(command)
         self.cmd = shlex.split(command)
         self.timeout = Helper.parse_time_to_secs(timeout)
         self.start_time = time.time()
 
-        self.outfile = 'stdout.txt'
-        self.errfile = 'stderr.txt'
-
-        #  self.p = subprocess.Popen(self.cmd,stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+        self.stdout = "No stdout captured yet"
+        self.stderr = "No stderr captured yet"
 
     def Run(self):
-        #  print("cmd: ", self.cmd)
-        self.p = subprocess.Popen(self.cmd,stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=False,
-                universal_newlines=True)
-        #  with open(self.outfile, 'w') as fout, open(self.errfile, 'w') as ferr:
-            #  self.p = subprocess.Popen(self.cmd,stderr=ferr, stdout=fout, shell=False)
+        #  self.p = subprocess.Popen(self.cmd,stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=False,
+        #          universal_newlines=True)
+
+        #  self.p = subprocess.Popen(self.cmd,stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=False,
+        #          universal_newlines=True)
+
+        self.p = subprocess.check_output(self.cmd)
+
+        # TODO: There is now a timeout parameter for check_output; weeh
+        # https://stackoverflow.com/questions/1191374/using-module-subprocess-with-timeout
+
 
         signal.signal(signal.SIGALRM, alarm_handler)
         signal.alarm(self.timeout)
 
-        self.stderr = "Can't capture output if terminated early (sorry, but I just spent 4h trying to get this)\n"
-        self.stderr += "Best option at this points seems to be the queue implementation here: \n"
-        self.stderr += "http://stackoverflow.com/questions/375427/non-blocking-read-on-a-subprocess-pipe-in-python\n"
-        self.stdout = self.stderr
+        #  self.stderr = "Can't capture output if terminated early (sorry, but I just spent 4h trying to get this)\n"
+        #  self.stderr += "Best option at this points seems to be the queue implementation here: \n"
+        #  self.stderr += "http://stackoverflow.com/questions/375427/non-blocking-read-on-a-subprocess-pipe-in-python\n"
+        #  self.stdout = self.stderr
 
         try:
             self.p.wait()
@@ -69,9 +63,12 @@ class ExpiringCommand(object):
             self.stderr = self.p.stderr.read()
             self.stdout = self.p.stdout.read()
         except Alarm:
-            #  print("cmd %s timed out; killing now" % (self.cmd))
             self.p.terminate()
 
+            so, se = self.p.communicate()
+            print "so:", so, se
+            #  so.seek(0)
+            print "so", so
             #this didn't work
             #  self.stdout = []
             #  self.stderr = []
@@ -80,8 +77,7 @@ class ExpiringCommand(object):
             #  for line in self.p.stderr:
             #      self.stderr.append(line)
 
-
-            #  self.returncode = -1
+            self.returncode = 1
         finally:
             #  if not self.stdout:
             #      self.stdout = self.p.stdout.read()
